20a84872627e84e5ea5d89c462abe71f2aec7445
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-05-08 18:05:03 +0530

    images here

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,12 +5,12 @@ const appUrl = process.env.NEXT_PUBLIC_URL;
 
 // frame preview metadata
 const appName = process.env.NEXT_PUBLIC_FRAME_NAME;
-const splashImageUrl = `${appUrl}/icon_replyguy.png`;
-const iconUrl = `${appUrl}/icon_replyguy.png`;
+const splashImageUrl = `${appUrl}/logo_replyguy.png`;
+const iconUrl = `${appUrl}/logo_replyguy.png`;
 
 const framePreviewMetadata = {
   version: "next",
-  imageUrl: `${appUrl}/opengraph.svg`,
+  imageUrl: `${appUrl}/opengraph.png`,
   button: {
     title: process.env.NEXT_PUBLIC_FRAME_BUTTON_TEXT,
     action: {
